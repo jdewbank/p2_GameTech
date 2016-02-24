15ec28b8f3bf8dddedd2d93a5a5dc16fbf84377b
r-escobar <rescobar713@gmail.com>
2016-02-24 10:50:54 -0600

    Added physics class

---
+++ Physics.h
@@ -0,0 +1,22 @@
+#include <btBulletDynamicsCommon.h>
+
+class Physics{
+
+	btDefaultCollisionConfiguration* collisionConfiguration;
+	btCollisionDispatcher* dispatcher;
+	btBroadphaseInterface* overlappingPairCache;
+	btSequentialImpulseConstraintSolver* solver;
+    btDiscreteDynamicsWorld* dynamicsWorld;
+	std::vector<btCollisionShape *> collisionShapes;
+	std::map<std::string, btRigidBody *> physicsAccessors;
+
+
+	Physics() {
+		collisionConfiguration = new btDefaultCollisionConfiguration();
+		dispatcher = new btCollisionDispatcher(collisionConfiguration);
+		overlappingPairCache = new btDbvtBroadphase();
+		solver = new btSequentialImpulseConstraintSolver();
+		dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+	}
+
+};
